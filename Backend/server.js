2f1af22b49ac55753b2bb90166ea98e4145cb694
HarshitRaj2712 <panditjee2712@gmail.com>
2026-02-26 10:19:58 +0530

    frontend url fixed

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
-      "https://your-frontend.vercel.app"
+      "https://smart-local-service.vercel.app"
     ],
     credentials: true,
   })
